7910879d44252838686092320ca23725f67dbd11
Igor Smit <74185879+Igorsmit00@users.noreply.github.com>
2024-05-07 15:25:53 +0200

    Initial commit online dispatching using FJSP-DRL

--- run_dispatching_rules.py
+++ run_dispatching_rules.py
@@ -4,32 +4,48 @@ import logging
 from data_parsers import parser_fajsp, parser_fjsp, parser_jsp_fsp
 from plotting.drawer import draw_gantt_chart, draw_precedence_relations
 from scheduling_environment.simulationEnv import SimulationEnv
-from solution_methods.helper_functions import load_parameters
 from solution_methods.dispatching_rules.scheduler import schedule_operations
+from solution_methods.FJSP_DRL.online_dispatching_helpers import (
+    load_dispatcher_FJSP_DRL, schedule_operations_drl)
+from solution_methods.helper_functions import load_parameters
 
 PARAM_FILE = "configs/dispatching_rules.toml"
 
 
 def run_method(simulationEnv, dispatching_rule, machine_assignment_rule):
     """Schedule simulator and schedule operations with the dispatching rules"""
-
+    if dispatching_rule == "FJSP_DRL":
+        dispatcher = load_dispatcher_FJSP_DRL()
     if simulationEnv.online_arrivals:
         # Start the online job generation process
         simulationEnv.simulator.process(simulationEnv.generate_online_job_arrivals())
 
         # Run the scheduling_environment until all operations are processed
         while True:
-            schedule_operations(simulationEnv, dispatching_rule, machine_assignment_rule)
+            if dispatching_rule == "FJSP_DRL":
+                schedule_operations_drl(simulationEnv, dispatcher)
+            else:
+                schedule_operations(
+                    simulationEnv, dispatching_rule, machine_assignment_rule
+                )
             yield simulationEnv.simulator.timeout(1)
 
     else:
+        if dispatching_rule == "FJSP_DRL":
+            raise ValueError(
+                "FJSP_DRL can only be used with online arrivals. For offline scheduling, use the dedicated FJSP_DRL script."
+            )
         # add machine resources to the environment
         for _ in simulationEnv.JobShop.machines:
             simulationEnv.add_machine_resources()
 
         # Run the scheduling env and schedule operations until all operations are processed from the data instance
-        while len(simulationEnv.processed_operations) < sum([len(job.operations) for job in simulationEnv.JobShop.jobs]):
-            schedule_operations(simulationEnv, dispatching_rule, machine_assignment_rule)
+        while len(simulationEnv.processed_operations) < sum(
+            [len(job.operations) for job in simulationEnv.JobShop.jobs]
+        ):
+            schedule_operations(
+                simulationEnv, dispatching_rule, machine_assignment_rule
+            )
             yield simulationEnv.simulator.timeout(1)
 
 
@@ -40,42 +56,62 @@ def main(param_file: str = PARAM_FILE):
         logging.error(f"Parameter file {param_file} not found.")
         return
 
-    simulationEnv = SimulationEnv(online_arrivals=parameters['instance']['online_arrivals'])
+    simulationEnv = SimulationEnv(
+        online_arrivals=parameters["instance"]["online_arrivals"]
+    )
 
-    if not parameters['instance']['online_arrivals']:
+    if not parameters["instance"]["online_arrivals"]:
         try:
-            if 'fjsp' in parameters['instance']['problem_instance']:
-                simulationEnv.JobShop = parser_fjsp.parse(simulationEnv.JobShop,
-                                                          parameters['instance']['problem_instance'])
-            elif 'fajsp' in parameters['instance']['problem_instance']:
-                simulationEnv.JobShop = parser_fajsp.parse(simulationEnv.JobShop,
-                                                           parameters['instance']['problem_instance'])
-            elif 'jsp' in parameters['instance']['problem_instance']:
-                simulationEnv.JobShop = parser_jsp_fsp.parse(simulationEnv.JobShop,
-                                                             parameters['instance']['problem_instance'])
+            if "fjsp" in parameters["instance"]["problem_instance"]:
+                simulationEnv.JobShop = parser_fjsp.parse(
+                    simulationEnv.JobShop, parameters["instance"]["problem_instance"]
+                )
+            elif "fajsp" in parameters["instance"]["problem_instance"]:
+                simulationEnv.JobShop = parser_fajsp.parse(
+                    simulationEnv.JobShop, parameters["instance"]["problem_instance"]
+                )
+            elif "jsp" in parameters["instance"]["problem_instance"]:
+                simulationEnv.JobShop = parser_jsp_fsp.parse(
+                    simulationEnv.JobShop, parameters["instance"]["problem_instance"]
+                )
         except Exception as e:
             print(f"Only able to schedule '/fjsp/, '/fasjp/ or '/jsp/' jobs': {e}")
 
         simulationEnv.simulator.process(
-            run_method(simulationEnv, parameters['instance']['dispatching_rule'],
-                           parameters['instance']['machine_assignment_rule']))
+            run_method(
+                simulationEnv,
+                parameters["instance"]["dispatching_rule"],
+                parameters["instance"]["machine_assignment_rule"],
+            )
+        )
         simulationEnv.simulator.run()
         logging.info(f"Makespan: {simulationEnv.JobShop.makespan}")
 
     else:
-        simulationEnv.set_online_arrival_details(parameters['online_arrival_details'])
-        simulationEnv.JobShop.set_nr_of_machines(parameters['online_arrival_details']['number_total_machines'])
-        simulationEnv.simulator.process(run_method(simulationEnv, parameters['instance']['dispatching_rule'],
-                                        parameters['instance']['machine_assignment_rule']))
-        simulationEnv.simulator.run(until=parameters['online_arrival_details']['simulation_time'])
+        simulationEnv.set_online_arrival_details(parameters["online_arrival_details"])
+        simulationEnv.JobShop.set_nr_of_machines(
+            parameters["online_arrival_details"]["number_total_machines"]
+        )
+        simulationEnv.simulator.process(
+            run_method(
+                simulationEnv,
+                parameters["instance"]["dispatching_rule"],
+                parameters["instance"]["machine_assignment_rule"],
+            )
+        )
+        simulationEnv.simulator.run(
+            until=parameters["online_arrival_details"]["simulation_time"]
+        )
 
-    if parameters['output']['plotting']:
+    if parameters["output"]["plotting"]:
         draw_precedence_relations(simulationEnv.JobShop)
         draw_gantt_chart(simulationEnv.JobShop)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Run (Online) Job Shop Simulation Model")
+    parser = argparse.ArgumentParser(
+        description="Run (Online) Job Shop Simulation Model"
+    )
     parser.add_argument(
         "-f",
         "--config_file",
